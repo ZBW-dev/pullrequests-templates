e8afa3eb9aa8d54a4e5f52b04c69196c75cb8922
Marcin Skrzyński <marcin.skrzynski@likims.com>
2021-02-24 20:39:05 +0100

    fix building extension

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,26 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
     '@prefresh/snowpack',
-    'snowpack-plugin-web-ext'
+    process.env.NODE_ENV === 'develop'
+      ? 'snowpack-plugin-web-ext'
+      : [
+          'snowpack-plugin-copy',
+          {
+            patterns: [
+              {
+                source: 'manifest.json',
+                destination: 'build',
+              },
+            ],
+          },
+        ],
   ],
   install: [
     /* ... */
@@ -20,7 +32,7 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    metaDir: "snowpack_meta" // chrome issue with __snowpack__ because _ is reserved for system
+    metaDir: 'snowpack_meta', // chrome issue with __snowpack__ because _ is reserved for system
     /* ... */
   },
   proxy: {
